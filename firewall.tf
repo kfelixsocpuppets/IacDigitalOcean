62947143021dd7d8adf64fb8f3f53eb833e0eefb
kfelixsocpuppets <59031696+kfelixsocpuppets@users.noreply.github.com>
2026-02-08 00:00:07 -0600

    Sample firewall tf

---
+++ firewall.tf
@@ -0,0 +1,64 @@
+resource "digitalocean_firewall" "mydropslets" {
+  name = "socpuppetsfirewall"
+  tags = [digitalocean_tag.fw_web.name]
+
+
+# sample rules for web drops specify with fw_web tag
+
+
+  # Allow SSH from anywhere
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "22"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  # Allow HTTP
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "80"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  # Allow HTTPS
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "443"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  # Allow HTTP_ALTERNATIVE
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "8080"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+
+  # Allow HTTP_RANGE
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "8082-8-8084"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+
+
+  # Allow all outbound traffic
+  outbound_rule {
+    protocol              = "tcp"
+    port_range            = "1-65535"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol              = "udp"
+    port_range            = "1-65535"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol              = "icmp"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+}
