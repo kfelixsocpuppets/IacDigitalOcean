f6f9f5882ae1ec1717dc05c7f35216b883a9ceb3
kfelixsocpuppets <59031696+kfelixsocpuppets@users.noreply.github.com>
2026-02-10 21:47:14 -0600

    Update main.tf
    redo main.tf with new fw tags

--- main.tf
+++ main.tf
@@ -1,9 +1,20 @@
-data "digitalocean_ssh_key" "macosx" {
+ data "digitalocean_ssh_key" "macosx" {
   name = "macosx"  # Use exact name from DO dashboard
 }
 
 data "digitalocean_ssh_key" "terraform" {
   name = "terraform"  # Use exact name from DO dashboard
+}
+
+
+
+
+resource "digitalocean_tag" "fw_sql" {
+  name = "fw_sql"
+}
+
+resource "digitalocean_tag" "fw_app" {
+  name = "fw_app"
 }
 
 
@@ -27,11 +38,12 @@ resource "digitalocean_droplet" "nyc" {
 # Droplet in San Francisco
 resource "digitalocean_droplet" "sfo" {
   image  = "ubuntu-24-04-x64"
-  name   = "web-sfo3"
+  name   = "db-sfo3"
   region = "sfo3"
   size   = "s-1vcpu-1gb"
   ipv6     = true               # Enable IPv6
   ssh_keys = [data.digitalocean_ssh_key.terraform.id]
-  depends_on = [digitalocean_firewall.mydropslets]
-  tags = [ "fw_web", "web", "production", "sfo"]
+  depends_on = [digitalocean_firewall.sql]
+  tags = [ "fw_sql", "fw_app", "production", "sfo"]
 }
+
